0844c85b05a1dd3d8cf5d204136d54168c1a08fb
chienpv-dgh <chienpv@digitechglobalco.com>
2020-06-15 20:15:10 +0700

    [Day 1: Data Types]

---
+++ day-1.js
@@ -0,0 +1,21 @@
+/* 
+  Day 1: Data Types
+*/
+
+// Declare second integer, double, and String variables.
+let first, second, third;
+
+// Read and save an integer, double, and String to your variables.
+first = parseInt(readLine());
+second = parseFloat(readLine());
+third = readLine();
+
+// Print the sum of both integer variables on a new line.
+console.log(i + first);
+
+// Print the sum of the double variables on a new line.
+console.log((d + second).toFixed(1));
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s + third);
